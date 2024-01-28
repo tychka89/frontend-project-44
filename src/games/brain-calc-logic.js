1b9fb878b36434251e6e5db9ff7ed0f812ad9765
Nataliya Rodikova <bachininang@gmail.com>
2024-01-29 02:50:55 +0500

    step10.3

--- src/games/brain-calc-logic.js
+++ src/games/brain-calc-logic.js
@@ -4,25 +4,25 @@ import getRandomNumber from '../random-number.js';
 const gameRules = 'What is the result of the expression?';
 const operations = ['+', '-', '*'];
 const calculate = (num1, num2, operator) => {
-  switch (operator) {
-  case '+':
-    return num1 + num2;
-  case '-':
-    return num1 - num2;
-  case '*':
-    return num1 * num2;
-  default:
-    return 'Неизвестный оператор';
-  }
+    switch (operator) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        default:
+            return 'Неизвестный оператор';
+    }
 };
 
 const setGame = () => {
-  const firstNumber = getRandomNumber(1, 10);
-  const secondNumber = getRandomNumber(1, 10);
-  const operator = operations[getRandomNumber(0, 2)];
-  const question = `${firstNumber} ${operator} ${secondNumber}`;
-  const wrightAnswer = `${calculate(firstNumber, secondNumber, operator)}`;
-  return [question, wrightAnswer];
+    const firstNumber = getRandomNumber(1, 10);
+    const secondNumber = getRandomNumber(1, 10);
+    const operator = operations[getRandomNumber(0, 2)];
+    const question = `${firstNumber} ${operator} ${secondNumber}`;
+    const wrightAnswer = `${calculate(firstNumber, secondNumber, operator)}`;
+    return [question, wrightAnswer];
 };
 
 const calcGame = () => game(gameRules, setGame);
