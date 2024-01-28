c994bda555c786ec9461c90aa5a0751df5571e1e
Nataliya Rodikova <bachininang@gmail.com>
2024-01-28 16:18:02 +0500

    step7

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 import evenGame from '../src/games/brain-even-logic.js';
 
 evenGame();
---
+++ src/games/brain-gcd-logic.js
@@ -0,0 +1,32 @@
+import readlineSync from 'readline-sync';
+import game from '../index.js';
+
+const gameRules = 'Find the greatest common divisor of given numbers.';
+const gameRounds = 3;
+
+const getRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+
+const gcd = (num1, num2) => {
+    while (num2 !== 0) {
+        let temp = num1 % num2;
+        num1 = num2;
+        num2 = temp;
+    }
+    return num1;
+};
+
+
+const setGame = () => {
+    const firstNumber = getRandomNumber(1, 100);
+    const secondNumber = getRandomNumber(1, 100);
+    const question = `${firstNumber} ${secondNumber}`;
+    const wrightAnswer = `${gcd(firstNumber, secondNumber)}`;
+    return [question, wrightAnswer];
+};
+
+const gcdGame = () => game(gameRules, setGame);
+
+export default gcdGame;
