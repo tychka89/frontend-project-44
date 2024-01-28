e199819c9f27c07c1d807951ca1d57528a5a7e31
Nataliya Rodikova <bachininang@gmail.com>
2024-01-29 01:58:59 +0500

    step10.1

--- src/games/brain-calc-logic.js
+++ src/games/brain-calc-logic.js
@@ -5,14 +5,14 @@ const gameRules = 'What is the result of the expression?';
 const operations = ['+', '-', '*'];
 const calculate = (num1, num2, operator) => {
     switch (operator) {
-        case '+':
-            return num1 + num2;
-        case '-':
-            return num1 - num2;
-        case '*':
-            return num1 * num2;
-        default:
-            return 'Неизвестный оператор';
+    case '+':
+        return num1 + num2;
+    case '-':
+        return num1 - num2;
+    case '*':
+        return num1 * num2;
+    default:
+        return 'Неизвестный оператор';
     }
 };
 
